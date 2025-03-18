f1606ff048866cf5f80754ed8b792dcc7c358a02
javiercuero <javierkuero@gmail.com>
2025-03-18 14:05:20 -0500

    Agregar script para ingresar y mostrar nombres y apellidos en orden inverso

---
+++ taller_1_mision_2.py
@@ -0,0 +1,5 @@
+'''
+Ingresar por teclado y por separado en dos variables, sus nombres y apellidos respectivamente,
+mostrar en orden contrario al que se ingresaron, es decir apellidos y nombres.
+'''
+print(input("ingrese sus nombres: "))
