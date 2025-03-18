9157b2cd15b7d1ae6af438db12d2e0d053013693
javiercuero <javierkuero@gmail.com>
2025-03-18 15:06:36 -0500

    Eliminar archivo antiguo y agregar nuevo script para ingresar y mostrar nombres y apellidos en orden inverso.

--- taller_1_mision_2.py
+++
@@ -1,5 +0,0 @@
-'''
-Ingresar por teclado y por separado en dos variables, sus nombres y apellidos respectivamente,
-mostrar en orden contrario al que se ingresaron, es decir apellidos y nombres.
-'''
-print(input("ingrese sus nombres: "))
---
+++ talleresMisionDos/tallerUno/puntoUno.py
@@ -0,0 +1,10 @@
+'''
+Ingresar por teclado y por separado en dos variables, sus nombres y apellidos respectivamente,
+mostrar en orden contrario al que se ingresaron, es decir apellidos y nombres
+'''
+# Definicio de variables y captura de datos
+nombres = input("Ingrese sus nombres: ")                    # Se capturan los nombres del usuario.
+apellidos = input("Ingrese sus apellidos: ")                # Se capturan los apellidos del usuario.
+
+# Salida de datos
+print("Sus apellidos y nombres son: ", apellidos, nombres)  # Se imprimen los apellidos y nombres del usuario.
