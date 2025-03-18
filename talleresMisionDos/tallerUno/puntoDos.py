d532739e3b7221a5b81349cbda6298230bd4b25d
javiercuero <javierkuero@gmail.com>
2025-03-18 15:39:09 -0500

    Agregar manejo de errores al calcular la edad de una persona y mejorar la entrada de datos.

--- talleresMisionDos/tallerUno/puntoDos.py
+++ talleresMisionDos/tallerUno/puntoDos.py
@@ -2,9 +2,11 @@
 Hallar la edad de una persona, conociendo su año de nacimiento; declarar una CONSTANTE con el año actual.
 '''
 # Definicion de variables y captura de datos
-anioActual = int(2025)                                                     # Se declara la constante con el año actual.
-anioNacimiento = int(input("Por favor ingrese su año de nacimiento: "))    # Se captura el año de nacimiento del usuario.
+anioActual = 2025                                                     # Se declara la constante con el año actual.
 
 # Procesamiento y salida de datos
-
-print(f"Su edad es: {anioActual - anioNacimiento} años.")                  # Se imprime la edad del usuario.
+try:
+     anioNacimiento = int(input("Por favor ingrese su año de nacimiento: "))    # Se captura el año de nacimiento del usuario.
+     print(f"Su edad es: {anioActual - anioNacimiento} años.")                  # Se imprime la edad del usuario.
+except ValueError:
+     print("Por favor ingrese un año valido.")                                  # Se imprime un mensaje de error si el año ingresado no es valido.
