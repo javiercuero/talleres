687bd35be5de83f64f5c3b52bab05fdd17235420
javiercuero <javierkuero@gmail.com>
2025-03-19 11:10:02 -0500

    Mejorar el script para leer un número entre 1 y 255, añadiendo manejo de errores y comentarios explicativos.

--- talleresMisionDos/tallerUno/puntoNueve.py
+++ talleresMisionDos/tallerUno/puntoNueve.py
@@ -1,19 +1,25 @@
 '''
 Leer un número de 1 al 255 y mostrar que carácter corresponde a su código ASCII, por ejemplo 64 muestra @.
 '''
-# Declarar la variable que almacenará el número ingresado por el usuario.
-
-num = int(input("Ingrese un número entre 1 y 255: "))
-
-# Se realizan los arreglos para verificar que el número ingresado se encuentre en el rango permitido.
+# Declarar la variable que almacenará el número ingresado por el usuario. y la impresion del mensaje.
 
 while True:
      try:
+          num = int(input("Ingrese un número entre 1 y 255: "))
+
           if 1 <= num <= 255:
                codeascii = chr(num)
-               print(f"El numero {num} corresponde al código ASCII: {codeascii}")
-               break
+               print(f"El número {num} corresponde al código ASCII: {codeascii}")
+               break  # Se sale del bucle si el usuario ingresa un número válido
           else:
-               print("El numero ingresado se encuentra fuera del rango permitido (entre 1 y 255), Intente nuevamente.")
+               print("El número ingresado está fuera del rango permitido (entre 1 y 255). Intente nuevamente.")
+
      except ValueError:
-          print("Error, Debe ingresar un número.")
+          print("Error!: Debe ingresar un número válido.")
+
+'''
+Notas del código:
+- Se utiliza un bucle while para solicitar al usuario que ingrese un número entre 1 y 255.
+- Se utiliza un bloque try-except para manejar errores en caso de que el usuario ingrese un valor no numérico.
+- Se verifica si el número ingresado está dentro del rango permitido (entre 1 y 255).
+'''
